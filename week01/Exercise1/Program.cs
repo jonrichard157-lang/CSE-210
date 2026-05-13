a2836fc08710655a277f70bd8e89f333322ec656
Joao Ricardo <seuemail@gmail.com>
2026-05-12 21:38:18 -0300

    exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -9,6 +9,6 @@ class Program
         Console.WriteLine("What is your last name?");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {firstName} {lastName}!");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}!");
     }
 }
